bb1b76c4dbce0ad3daf3c4ec194d2c6452344e64
andrasolah <andras.olah.2@gmail.com>
2025-04-28 12:35:20 +0200

    input validations and more menu logics added

--- main.py
+++ main.py
@@ -51,28 +51,57 @@ class BookSystem:
                     return flight
         return None
     
-    def book_ticket_manually(self):
-        flight_number = input("Enter flight number: ")
-        flight = self.find_flight_by_number(flight_number)
-        if flight:
-            if flight.departure_datetime < datetime.now():
-                print("Cannot book a ticket for a flight that has already departed.")
-                return
-            ticket_id = len(self.tickets) + 1
-            while True:
-                passenger_name = input("Enter passenger name (or 'exit' to cancel): ").strip()
-                if passenger_name.lower() == "exit":
-                    return
-                if not passenger_name:
-                    print("Passenger name cannot be empty. Please try again.")
-                    continue
-                break
-            ticket = Ticket(ticket_id, passenger_name, flight)
-            self.tickets.append(ticket)
-            print(f"Ticket booked successfully! Ticket ID: {ticket_id}")
-        else:
-            print("Flight not found.")
+    def find_ticket_by_id(self, ticket_id: int):
+        for ticket in self.tickets:
+            if ticket.ticket_id == ticket_id:
+                return ticket
+        return None
     
+    def reset_airline_console(self):
+        clear_console()
+        self.display_airlines(True)
+        
+    def reset_ticket_console(self):
+        clear_console()
+        self.list_tickets()
+
+    def book_ticket_manually(self):
+        while True:
+            try:
+                flight_number = input("Enter flight number (or '0' to main menu): ")
+                if flight_number == "0":
+                    self.reset_airline_console()
+                    return
+                flight = self.find_flight_by_number(flight_number)
+                if flight:
+                    if flight.departure_datetime < datetime.now():
+                        self.reset_airline_console()
+                        print("Cannot book a ticket for a flight that has already departed.")
+                        continue
+                    ticket_id = len(self.tickets) + 1
+                    while True:
+                        passenger_name = input(f"Enter passenger name for flight {flight_number} (or '0' to cancel and change flight number): ").strip()
+                        if passenger_name == '0':
+                            self.reset_airline_console()
+                            break
+                        if not passenger_name:
+                            self.reset_airline_console()
+                            print("Passenger name cannot be empty. Please try again.")
+                            continue
+                        break
+                    if passenger_name and passenger_name != '0':
+                        ticket = Ticket(ticket_id, passenger_name, flight)
+                        self.tickets.append(ticket)
+                        self.reset_airline_console()
+                        print(f"Ticket for for flight {flight_number} booked successfully! Ticket ID: {ticket_id}")
+                else:
+                    self.reset_airline_console()
+                    print("Flight not found.")
+            except ValueError:
+                self.reset_airline_console()
+                print("Invalid input. Please enter a valid flight number.")
+                continue
+
     def book_ticket(self,passenger_name=None,flight_number=None):
         if passenger_name and flight_number:
             ticket_id = len(self.tickets) + 1
@@ -92,19 +121,25 @@ class BookSystem:
         return True
     
     def cancel_ticket(self):
-        try:
-            ticket_id = int(input("Enter ticket ID to cancel: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid ticket ID.")
-            return
-
-        for ticket in self.tickets:
-            if ticket._ticket_id == ticket_id:
+        while True:
+            try:
+                ticket_id = int(input("Enter ticket ID to cancel (or '0' to main menu): "))
+                if ticket_id == 0:
+                    self.reset_airline_console()
+                    return
+            except ValueError:
+                self.reset_ticket_console()
+                print("Invalid input. Please enter a valid ticket ID.")
+                continue
+            ticket = self.find_ticket_by_id(ticket_id)
+            if ticket:
                 self.tickets.remove(ticket)
+                self.reset_ticket_console()
                 print(f"Ticket ID {ticket_id} cancelled successfully.")
-                return
+            else:
+                self.reset_ticket_console()
+                print("Ticket not found. Please enter a valid ticket ID.")
 
-        print("Ticket not found. Please enter a valid ticket ID.")
         
     def user_interface(self):
         while True:
@@ -112,32 +147,27 @@ class BookSystem:
             print("2. Book a ticket")
             print("3. List tickets")
             print("4. Cancel a ticket")
-            print("5. Exit")
+            print("0. Exit")
             try:
                 choice = int(input("Enter your choice: "))
                 if choice == 1:
-                    clear_console()
-                    self.display_airlines(True)
+                    self.reset_airline_console()
                 elif choice == 2:
-                    clear_console()
-                    self.display_airlines(True)
+                    self.reset_airline_console()
                     self.book_ticket()
                 elif choice == 3:
-                    clear_console()
-                    self.list_tickets()
+                    self.reset_ticket_console()
                 elif choice == 4:
                     clear_console()
                     if self.list_tickets(): 
                         self.cancel_ticket()
-                elif choice == 5:
+                elif choice == 0:
                     break
                 else:
-                    clear_console()
-                    self.display_airlines(True)
+                    self.reset_airline_console()
                     print("Invalid choice. Please try again.")
             except ValueError:
-                clear_console()
-                self.display_airlines(True)
+                self.reset_airline_console()
                 print("Invalid input. Please enter a valid input.")
     
 def clear_console():
